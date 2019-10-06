1664ff5e58700b30770c9ffcf60ce33521dcc1a9
dafnamargalit <dama7429@colorado.edu>
2019-10-06 10:07:08 -0600

    d 2

--- DesignBlock2/Complete/FourBitAdder.v
+++ DesignBlock2/Complete/FourBitAdder.v
@@ -1,41 +1,26 @@
 module FourBitAdder(
 input [3:0] inputx,
 input [3:0] inputy,
-input carryin,
 output [9:0] SW,
 output [1:0] KEY,
-output overflow
+output overflow,
+output [3:0] sum
 );
 
 wire [3:0] input_x;
 wire [3:0] input_y;
-wire [3:0] carry_in;
-wire [3:0] sum;
+wire [3:0] sum_out;
 wire [3:0] carry_out;
+
 assign input_x = SW[7:4];
+assign input_y = ~KEY[0] ? SW[3:0] : ~SW[3:0];
 
-
-always @(KEY)
-begin
-    if(KEY[0])
-    begin
-        assign input_y = SW[3:0];
-    end
-    if(~KEY[0])
-    begin
-        assign input_y = ~SW[3:0];
-    end
-end
-
-FullAdder adder1(.input1 (input_x[0]), .input2 (input_y[0]), .carry_in (~KEY[0]), .sum (sum[0]), .carry_out (carry_out[0]));
-FullAdder adder2(.input1 (input_x[1]), .input2 (input_y[1]), .carry_in (carry_out[0]), .sum (sum[1]), .carry_out (carry_out[1]));
-FullAdder adder3(.input1 (input_x[2]), .input2 (input_y[2]), .carry_in (carry_out[1]), .sum (sum[2]), .carry_out (carry_out[2]));
-FullAdder adder4(.input1 (input_x[3]), .input2 (input_y[3]), .carry_in (carry_out[2]), .sum (sum[3]), .carry_out (carry_out[3]));
+FullAdder adder1(.input1 (input_x[0]), .input2 (input_y[0]), .carry_in (~KEY[0]), .sum (sum_out[0]), .carry_out (carry_out[0]));
+FullAdder adder2(.input1 (input_x[1]), .input2 (input_y[1]), .carry_in (carry_out[0]), .sum (sum_out[1]), .carry_out (carry_out[1]));
+FullAdder adder3(.input1 (input_x[2]), .input2 (input_y[2]), .carry_in (carry_out[1]), .sum (sum_out[2]), .carry_out (carry_out[2]));
+FullAdder adder4(.input1 (input_x[3]), .input2 (input_y[3]), .carry_in (carry_out[2]), .sum (sum_out[3]), .carry_out (carry_out[3]));
 
 assign overflow = carry_out[2] ^ carry_out[3];
-
+assign sum = sum_out;
 endmodule
 
-//if overflow, of
-// if positive, positive
-//if negative, negative sign, two's complement
